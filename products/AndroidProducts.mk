b8d166e17a58e17a95bfc60fb43034767f8a2e6a
Mitch Davis <mitchelldavis44@gmail.com>
2017-01-18 18:49:38 -0600

    Initial vendor

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,4 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/platinum_hammerhead.mk \
+    $(LOCAL_DIR)/platinum_shamu.mk
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo platinum_hammerhead-userdebug
+add_lunch_combo platinum_shamu-user
